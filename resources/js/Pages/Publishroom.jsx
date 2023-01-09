a61892d32efd023d2ef410e8a2450efd2e63b79d
Martti Syber <marttisyber@gmail.com>
2023-01-09 20:31:27 +0300

    first commit

---
+++ resources/js/Pages/Publishroom.jsx
@@ -0,0 +1,13 @@
+import { Link, Head } from '@inertiajs/inertia-react';
+
+export default function Publishroom(props) {
+    return (
+        <>
+            <Head title="Publish Room" />
+
+            <div class="header">
+                12343
+            </div>
+        </>
+    );
+}
