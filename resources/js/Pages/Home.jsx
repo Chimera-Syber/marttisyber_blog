433963bb5a2011795214d1ae3f9fdb044bf38738
Martti Syber <marttisyber@gmail.com>
2023-01-21 17:57:25 +0300

    WIP(Database) Add avatar and softDelete at user table

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,11 +1,14 @@
+import ApplicationLogo from "@/Components/ApplicationLogo";
+
 export default function Home(props) {
     return (
         <>
             <header class="header">
-                <div class="container">
-                    123231
+                <div class="blog-container">
+                    <nav class="navbar">
+                        <ApplicationLogo/>
+                    </nav>
                 </div>
-
             </header>
         </>
     )
